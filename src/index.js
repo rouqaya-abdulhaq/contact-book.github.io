22235d9716e5e018333868aa17e8ebb575cc64bc
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-06-09 17:42:18 +0300

    wrote a synopsis for the program.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import Card from "./card.js";
+import Contact from "./contacts";
+/*synopsis:
+    the user should sign up or log in and then be directed to the contact list
+    where he can add , edit and delete contacts.
+    
+    when the user presses the add button in the contact list, a pop up card with a form
+    will appear to register the contact information and a save button will save it.
+    
+    once saved the new contact's full name will appear in the list with an edit and delete 
+    buttons beside it.
+    
+    if the user presses the edit button the pop up card will reappear with the contact's 
+    information already in place ready to be adjusted
+    
+    if the delete button is pressed the user will be warned if he is sure about the delete
+    
+    the color palette should be able to change the page's style at any point of the programm*/
