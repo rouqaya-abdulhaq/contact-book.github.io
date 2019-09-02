37400a9d73bcf88d7980a3add77682e954e4ce19
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-09-02 22:39:09 +0300

    palnned some ideas for project.

--- src/contacts.js
+++ src/contacts.js
@@ -1,17 +1,19 @@
 import Palette from "./palette";
 import Information from "./information.js";
 
+const addButton = document.querySelector("#addButton");
+const contactList = document.querySelector("#contactsList");
 export class Contact {
+    //need a way to access the methods
     createContact(){
-        let card = document.createElement("div");
-        card.setAttribute("class", "card");
-        return card;
+        let contact = document.createElement("div");
+        contact.setAttribute("class", "card");
+        contact.innerText("hello");
+        contactList.appendChild(contact);
     }
     submiteInformation(information){
         //
     }
-    chooseStyle(){
-        //
-    }
 } 
 
+addButton.addEventListener("click",()=>console.log("clicked"));
