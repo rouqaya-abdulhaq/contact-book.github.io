983599cad734035af19f4653131d41a18f8b7499
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-09-10 21:54:36 +0300

    added delete button to contact

--- src/contacts.js
+++ src/contacts.js
@@ -9,12 +9,18 @@ const contactList = document.querySelector("#contactsList");
         contact.innerText = info;
         contactList.appendChild(contact);
         contactList.appendChild(this.createEditBtn());
+        contactList.appendChild(this.createDeleteBtn());
     }
     createEditBtn(){
         let editImg = document.createElement("IMG");
         editImg.setAttribute("src","src/icon.png");
         editImg.setAttribute("alt","edit icon");
         return editImg; 
+    }
+    createDeleteBtn(){
+        let deleteBtn = document.createElement("span");
+        deleteBtn.innerText = " x ";
+        return deleteBtn;
     }
     //submit the form to the data storage and have a different method that displays 
     //the contact to the contact list
