b387aaef35469430822f106fcaa4b2b93ec72d84
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-09-28 18:20:18 +0300

    cleaned up contactInfoDisplay

--- src/contactInfoDisplay.js
+++ src/contactInfoDisplay.js
@@ -1,30 +1,45 @@
-//break into smaller methods
-export const displayContactInfo = (container,contactInfo) => {
-    const infoCard = createInfoCard(contactInfo);
-    const cancelBtn = document.createElement("button");
-    cancelBtn.innerText = " X ";
-    cancelBtn.addEventListener("click",()=>removeCard(container,infoCard));
-    infoCard.appendChild(cancelBtn);
+export const displayContactInfo = (container,contactInfo)=>{
+    const infoCard = createInfoCard(container,contactInfo);
     container.appendChild(infoCard);
 }
 
-const removeCard = (container,card) =>{
-    container.removeChild(card);
+const createInfoCard = (container,contactInfo) =>{
+    const infoCard = createInfoCardElement();
+    const info = createInfo(contactInfo);
+    const cancelBtn = createCancelBtn(container,infoCard);
+    infoCard.append(cancelBtn,info);
+    return infoCard;
 }
 
-const createInfoCard = (contactInfo)=>{
+const createInfoCardElement = () =>{
     const infoCard = document.createElement("div");
     infoCard.setAttribute("id","infoCard");
+    return infoCard;
+}
+
+const createInfo = (contactInfo) =>{
+    const infoWrap = document.createElement("div");
     const firstName = createInfoPH("First Name",contactInfo.firstName);
     const lastName = createInfoPH("Last Name",contactInfo.lastName);
     const email = createInfoPH("Email",contactInfo.email);
     const phoneNumber = createInfoPH("Phone Number",contactInfo.phoneNumber);
-    infoCard.append(firstName,lastName,email,phoneNumber);
-    return infoCard;
+    infoWrap.append(firstName,lastName,email,phoneNumber);
+    return infoWrap;
 }
 
 const createInfoPH = (label,value) =>{
     const ph = document.createElement("span");
     ph.innerText = label + " : " + value;
     return ph;
+}
+
+const createCancelBtn = (container,infoCard) =>{
+    const cancelBtn = document.createElement("button");
+    cancelBtn.innerText = " X ";
+    cancelBtn.addEventListener("click",()=>removeCard(container,infoCard));
+    return cancelBtn;
+}
+
+const removeCard = (container,card) =>{
+    container.removeChild(card);
 }
