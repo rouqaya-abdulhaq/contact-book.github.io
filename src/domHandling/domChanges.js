193e809a042b082b0dcd0446133ab971c1d73a5f
Rouqaya Abdulhaq <ruqia334@gmail.com>
2019-11-19 20:08:57 +0300

    deleted commented remove info card code

--- src/domHandling/domChanges.js
+++ src/domHandling/domChanges.js
@@ -1,13 +1,5 @@
 import {contactMain} from './domElements';
 
-// export const removeFormFromDom = () =>{
-//     const form = document.querySelector("form");//should i put this in the domElements??
-//     removeChildFromContactMain(form);
-// }
-
-export const removeInfoCardFromDom = () =>{
-    //should i remove it here??
-}
 
 export const appendToMain = (elem) =>{
     contactMain.append(elem);
