cff1c3e6019ee3a5559b573403a6dd7f0b7902f3
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-03-02 21:28:56 +0300

    written validation methods

--- src/utilities/validateInput.js
+++ src/utilities/validateInput.js
@@ -14,17 +14,29 @@ export const validateInput = (input) => {
 }
 
 const isName = (value) => {
-    return true;
+    if(value.match(/^[A-Za-z]+$/)){
+        return true;
+    }
+    return false;
 }
 
 const isEmail = (value) => {
-    return true;
+    if(value.match(/\S+@\S+\.\S+/)){
+        return true;
+    }
+    return false;
 }
 
 const isPhoneNumber = (value) => {
-    return true;
+    if(value.match(/^[(]?[0-9]{3}[)]?[-\s]?[0-9]{3}[-\s]?[0-9]{4,6}$/)){
+        return true;
+    }
+    return false;
 }
 
 const isPassword = (value) => {
-    return true;
+    if(value.match(/^\w+$/)){
+        return true;
+    }
+    return false;
 }
