50e158c7776c8eb36ef864229c2ef54a87645240
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-05 21:01:20 +0300

    enhanced creating infoCard with p elements

--- src/contacts/contactInfoCard.js
+++ src/contacts/contactInfoCard.js
@@ -1,34 +1,62 @@
 export const createInfoCard = (contactInfo) =>{
-    const card = createInfoCardElement();
+    const card = createCardElement();
     const info = createInfo(contactInfo);
     const cancelBtn = createCancelBtn();
     card.append(cancelBtn,info);
     return card;
 }
 
-const createInfoCardElement = () =>{
+const createCardElement = () =>{
     const infoCard = document.createElement("div");
-    infoCard.setAttribute("id","infoCard");
+    infoCard.setAttribute("class","contactPopUps");
     return infoCard;
 }
 
 const createInfo = (contactInfo) =>{
-    const info = assignInfo(contactInfo);
+    const infoObj = contactInfoObj(contactInfo)
     const infoWrap = document.createElement("div");
     infoWrap.setAttribute("id","infoWrap");
-    infoWrap.innerText = info;
+    assignInfoToElems(infoObj,infoWrap);
     return infoWrap;
 }
 
-const assignInfo = (contactInfo) => {
-    const firstName = createInfoPH("First Name",contactInfo.firstName);
-    const lastName = createInfoPH("Last Name",contactInfo.lastName);
-    const email = createInfoPH("Email",contactInfo.email);
-    const phoneNumber = createInfoPH("Phone Number",contactInfo.phoneNumber);
-    return (firstName +lastName+email+phoneNumber);
+const contactInfoObj = (contactInfo) =>{
+    return({
+        firstName : {
+            label : "First Name",
+            value : contactInfo.firstName
+        },
+        lastName : {
+            label : "Last Name",
+            value : contactInfo.lastName
+        },
+        email : {
+            label : "Email",
+            value : contactInfo.email
+        },
+        phoneNumber : {
+            label : "Phone Number",
+            value : contactInfo.phoneNumber
+        },
+    });
 }
 
-const createInfoPH = (label,value) =>{
+const assignInfoToElems = (contactInfo,infoWrap) => {
+    for (const info in contactInfo){
+       const pElement = createSingleInfoElement({label : contactInfo[info].label
+        ,value : contactInfo[info].value});
+       infoWrap.append(pElement);
+    }
+}
+
+const createSingleInfoElement = (info) =>{
+    const infoElem = document.createElement("p");
+    const infoText = formatInfoInnerTxt(info.label,info.value);
+    infoElem.innerText = infoText;
+    return infoElem;
+}
+
+const formatInfoInnerTxt = (label,value) =>{
     return label + " : " + value + " ";
 }
 
