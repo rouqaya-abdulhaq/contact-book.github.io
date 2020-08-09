1757e51197aa4b8fbd09758cad5493807bceae10
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-08-09 20:23:32 +0300

    changed style back to default on sign out

--- src/actions/registartion/onSignOut.js
+++ src/actions/registartion/onSignOut.js
@@ -4,16 +4,22 @@ import {updateUser} from '../../registration/user';
 
 const contactList = document.querySelector("#contactsList");
 const title = document.querySelector(".title");
+const body = document.querySelector("body");
 
 export const onSignOut = () =>{
     updateUser(null);
     clearContactList();
+    changeStyleToDefault();
     goToSignIn();
 }
 
 const clearContactList = () =>{
     contactList.innerHTML = "";
     title.innerText = "";
+}
+
+const changeStyleToDefault = () =>{
+    body.className = "default";
 }
 
 const goToSignIn = () =>{
